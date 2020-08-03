051394ab6302571ae0fa62845c61527babe55ba4
TomatoPope0 <tomatopope0@gmail.com>
2020-08-03 21:55:54 +0900

    Add save images feature to generation tool

--- Generate.py
+++ Generate.py
@@ -1,11 +1,20 @@
+import argparse 
+import os.path as path
 import torch
 import torchvision.transforms as transforms
 from ModelGAN import Generator
 
-G = Generator()
-G.load_state_dict(torch.load("./Model/G.pt"))
+parser = argparse.ArgumentParser()
+parser.add_argument("model", type=str)
+parser.add_argument("output", type=str)
+parser.add_argument("--num-images", "-n", type=int)
+args = parser.parse_args()
 
-z = torch.randn(100)
-y = G(z).view(28, 28)
-y_image = transforms.ToPILImage()(y)
-y_image.show()
+G = Generator()
+G.load_state_dict(torch.load(args.model))
+
+for i in range(args.num_images):
+    z = torch.randn(100)
+    y = G(z).view(28, 28)
+    y_image = transforms.ToPILImage()(y)
+    y_image.save(path.join(args.output, "image%d.bmp" % i))
